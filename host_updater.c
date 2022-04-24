9700cdad40c91ca6f0048bbe87864fddb95dab4c
AsimIkram <aikram96@gmail.com>
2022-04-24 18:54:12 -0500

    Multiple directorieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeees handled

--- host_updater.c
+++ host_updater.c
@@ -1,10 +1,40 @@
- #define _GNU_SOURCE
+#define _GNU_SOURCE
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+#include<dirent.h>
+#include<sys/stat.h>
 
 #define buff_len 255
 char* entries[2];
+char* host_files[255];                      // Paths to hosts files of containers
+
+void get_directories(char* parent_dir) {
+    int i=0;
+    struct dirent* dir_entry;
+    DIR* src = opendir(parent_dir);
+    while((dir_entry = readdir(src)) != NULL)
+    {
+        struct stat st;
+        if(strcmp(dir_entry->d_name, ".") != 0 && strcmp(dir_entry->d_name, "..") != 0) {
+            if (fstatat(dirfd(src), dir_entry->d_name, &st, 0) < 0)
+            {
+                perror(dir_entry->d_name);
+                continue;
+            }
+            if(S_ISDIR(st.st_mode)) {
+                host_files[i] = (char*)malloc(255 * sizeof(char));
+                sprintf(host_files[i], dir_entry->d_name);
+                sprintf(host_files[i] + strlen(host_files[i]), "/hosts");
+                printf("%s\n", host_files[i]);
+                i++;
+            }
+        }
+        
+        
+    }
+    closedir(src);
+}
 
 char* remove_spaces(char* input_string) {
     int j=0;
@@ -58,10 +88,16 @@ void write_to_hosts(FILE* read_path) {
 }
 
 int main() {
-    char* host_path = "/home/ubuntu/522/ContainerDNS/test/hosts";
-    FILE *read_fp  = fopen(host_path, "r+");
-    write_to_hosts(read_fp);
-    fclose(read_fp);
+    char* host_path = "/home/ubuntu/522/ContainerDNS/test/";
+    get_directories(host_path);
+    while(1) {
+        for(int i=0; i<2; i++) {
+            printf("%s\n", host_files[i]);
+            FILE *read_fp  = fopen(host_files[i], "r+");
+            write_to_hosts(read_fp);
+            fclose(read_fp);
+        }
+    }
 
     return 0;
 }
