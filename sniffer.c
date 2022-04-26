3b9b550e8aa145c2c05213088e316942cb8ab7f7
AsimIkram <aikram96@gmail.com>
2022-04-26 05:09:34 -0500

    sniffer updated

--- sniffer.c
+++ sniffer.c
@@ -1,8 +1,57 @@
 #include<stdio.h>
 #include<pcap.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
 
-void packet_processor() {
-    printf("Packet\n");
+void packet_processor(u_char* args, const struct pcap_pkthdr *header, const u_char *packet) {
+    // Referenced from: https://www.devdungeon.com/content/using-libpcap-c
+    struct ether_header *eth_header;
+    eth_header = (struct ether_header *) packet;
+    if (ntohs(eth_header->ether_type) != ETHERTYPE_IP) {
+        printf("Not an IP packet. Skipping...\n\n");
+        return;
+    }
+    const u_char *ip_header;
+    const u_char *udp_header;
+    const u_char *payload;
+
+    int ethernet_header_length = 14; 
+    int ip_header_length;
+    int udp_header_length;
+    int payload_length;
+
+    ip_header = packet + ethernet_header_length;
+    ip_header_length = ((*ip_header) & 0x0F);
+    ip_header_length = ip_header_length * 4;
+    u_char protocol = *(ip_header + 9);
+    if (protocol != IPPROTO_UDP) {
+        printf("Not a UDP packet. Skipping...\n");
+        return;
+    }
+
+    udp_header = packet + ethernet_header_length + ip_header_length;
+    // Length should be at starting at the 5th byte
+    udp_header_length = ((*(udp_header + 10)) & 0xF0) >> 4;
+    udp_header_length = udp_header_length * 4;
+    printf("UDP header length in bytes: %d\n", udp_header_length);
+
+    // Get payload offset
+    int total_headers_size = ethernet_header_length+ip_header_length+udp_header_length;
+    printf("Size of all headers combined: %d bytes\n", total_headers_size);
+    payload_length = header->caplen - (ethernet_header_length + ip_header_length + udp_header_length);
+    printf("Payload size: %d bytes\n", payload_length);
+    payload = packet + total_headers_size;
+    printf("Memory address where payload begins: %p\n\n", payload);
+
+    if (payload_length > 0) {
+        const u_char *temp_pointer = payload;
+        int byte_count = 0;
+        while (byte_count++ < payload_length) {
+            printf("%c", *temp_pointer);
+            temp_pointer++;
+        }
+        printf("\n");
+    }
 }
 
 void sniff_packet() {
